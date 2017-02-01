e744af66b9ce8c8e8984ae34decc6452c9825caa
Vladimir Rubin <vladimir.rubin@qweeco.com>
2017-02-01 16:00:05 +0300

    First test run :)

--- handlers/pull_request.js
+++ handlers/pull_request.js
@@ -10,66 +10,65 @@ var pull_request_handler = function (data) {
     if (['synchronize', 'opened', 'reopened'].indexOf(data.action) >= -1) {
         result = {
             after_commit_hash: data.pull_request.head.sha,
-            owner: data.pull_request.head.repo.owner.login,
-            repository: data.repository.full_name,
+            owner: data.repository.owner.login,
+            repository: data.repository.name,
             author: data.pull_request.user.login,
             diff_url: data.pull_request.diff_url,
             results: {},
             extra_results: {},
             pr_number: data.number,
         }
-        console.log(result);
-        // // Get differences
-        // utils.getGitHubFilePromise(result.diff_url)
-        //     .then(fileData => {
-        //         // Check PEP8, ESLint
-        //         const filesToCheck = utils.getFilesFromDiff(fileData).map(file => file.to);
-        //         const onlyJsFiles = filesToCheck.filter(utils.regExpFilter(/.js/));
-        //         fs.mkdir(CHECKED_DIR, () => {
-        //             onlyJsFiles.forEach(file => {
-        //                 const outputFilename = file.split('/').slice(-1)[0];
-        //                 const fileStream = fs.createWriteStream(`${CHECKED_DIR}/${outputFilename}`);
-        //                 const requestOptions = utils.getRawGitHubOptions({
-        //                      owner: result.owner,
-        //                      repository: result.repository,
-        //                      sha: result.after_commit_hash,
-        //                      filename: file,
-        //                 });
-        //                 const request = http.get(requestOptions, function (response) {
-        //                     response.pipe(fileStream);
-        //                     response.on('end', () => {
-        //                         // Prepare message
-        //                         const report = Object.assign(utils.checkEslint([`${CHECKED_DIR}/${outputFilename}`], { inputFilename: file }));
-        //                         const comment = utils.prepareComment(report);
-        //                         const commentText = comment.header + comment.body;
-        //                         // Send Message
-        //                         const commentRequestOptions = {
-        //                             protocol: 'https:',
-        //                             host: 'api.github.com',
-        //                             method: 'POST',
-        //                             path: `/repos/${result.author}/${result.repository}/issues/${result.pr_number}/comments`,
-        //                             headers: utils.getGitHubHeaders(),
-        //                         }
-        //                         var req = http.request(commentRequestOptions, function (res) {
-        //                             var chunks = [];
-        //                             res.on("data", function (chunk) {
-        //                                 chunks.push(chunk);
-        //                             });
-        //                             res.on("end", function () {
-        //                                 var body = Buffer.concat(chunks);
-        //                                 console.log(body.toString());
-        //                             });
-        //                         });
-        //                         req.write(JSON.stringify({ body: commentText }));
-        //                         req.end();
-        //                         rmdirSync(CHECKED_DIR);
-        //                     });
-        //                 });
-        //             });
-        //         });
+        // Get differences
+        utils.getGitHubFilePromise(result.diff_url)
+            .then(fileData => {
+                // Check PEP8, ESLint
+                const filesToCheck = utils.getFilesFromDiff(fileData).map(file => file.to);
+                const onlyJsFiles = filesToCheck.filter(utils.regExpFilter(/.js/));
+                fs.mkdir(CHECKED_DIR, () => {
+                    onlyJsFiles.forEach(file => {
+                        const outputFilename = file.split('/').slice(-1)[0];
+                        const fileStream = fs.createWriteStream(`${CHECKED_DIR}/${outputFilename}`);
+                        const requestOptions = utils.getRawGitHubOptions({
+                             owner: result.owner,
+                             repository: result.repository,
+                             sha: result.after_commit_hash,
+                             filename: file,
+                        });
+                        const request = http.get(requestOptions, function (response) {
+                            response.pipe(fileStream);
+                            response.on('end', () => {
+                                // Prepare message
+                                const report = Object.assign(utils.checkEslint([`${CHECKED_DIR}/${outputFilename}`], { inputFilename: file }));
+                                const comment = utils.prepareComment(report);
+                                const commentText = comment.header + comment.body;
+                                // Send Message
+                                const commentRequestOptions = {
+                                    protocol: 'https:',
+                                    host: 'api.github.com',
+                                    method: 'POST',
+                                    path: `/repos/${result.owner}/${result.repository}/issues/${result.pr_number}/comments`,
+                                    headers: utils.getGitHubHeaders(),
+                                }
+                                var req = http.request(commentRequestOptions, function (res) {
+                                    var chunks = [];
+                                    res.on("data", function (chunk) {
+                                        chunks.push(chunk);
+                                    });
+                                    res.on("end", function () {
+                                        var body = Buffer.concat(chunks);
+                                        console.log(body.toString());
+                                    });
+                                });
+                                req.write(JSON.stringify({ body: commentText }));
+                                req.end();
+                                rmdirSync(CHECKED_DIR);
+                            });
+                        });
+                    });
+                });
 
-        //     })
-        //     .catch(e => console.error(e));
+            })
+            .catch(e => console.error(e));
     }
     return result;
 }
